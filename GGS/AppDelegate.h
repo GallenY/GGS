db33b98a48e8ed754953bb500152779e209fe7bc
GallenY <Gallen@gaoyangdeMacBook-Pro.local>
2015-11-05 10:40:55 +0800

    init

---
+++ GGS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GGS
+//
+//  Created by 高阳 on 15/11/5.
+//  Copyright © 2015年 高阳. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GGS/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GGS
+//
+//  Created by 高阳 on 15/11/5.
+//  Copyright © 2015年 高阳. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
